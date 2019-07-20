db50666d747c353758caf3f4cc4eff267b11d932
Gideon-Muriithi <gideongakenge@gmail.com>
2019-07-20 23:48:03 +0300

    Add toggle and show js functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $(".clickable").click(function(){
+    $(".toggle").toggle();
     $("#text-hidden").show();
+  });
+  $(".clickable1").click(function(){
+    $(".toggle1").toggle();
+    $("#text-hidden1").show();
   });
 
 });
